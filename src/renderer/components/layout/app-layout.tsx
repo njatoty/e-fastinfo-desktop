c34c50306e6b8a5230ff07e7a3c67e5b4e155de9
njatoty <njatotianafiononana@gmail.com>
2025-06-05 20:29:57 +0300

    change radius

--- src/renderer/components/layout/app-layout.tsx
+++ src/renderer/components/layout/app-layout.tsx
@@ -6,18 +6,23 @@ import { cn } from '@/lib/utils';
 
 export function AppLayout() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
-  
+
   return (
     <div className="min-h-screen bg-background">
       <Sidebar open={sidebarOpen} onOpenChange={setSidebarOpen} />
-      
-      <div className={cn(
-        "flex flex-col min-h-screen transition-all duration-300 ease-in-out",
-        sidebarOpen ? "lg:pl-64" : "lg:pl-16"
-      )}>
-        <Header sidebarOpen={sidebarOpen} onSidebarOpenChange={setSidebarOpen} />
-        
-        <main className="flex-1 p-4 md:p-6">
+
+      <div
+        className={cn(
+          'flex flex-col min-h-screen transition-all duration-300 ease-in-out',
+          sidebarOpen ? 'lg:pl-64' : 'lg:pl-16'
+        )}
+      >
+        <Header
+          sidebarOpen={sidebarOpen}
+          onSidebarOpenChange={setSidebarOpen}
+        />
+
+        <main className="flex-1 p-4 md:p-6 bg-gray-100">
           <Outlet />
         </main>
       </div>
